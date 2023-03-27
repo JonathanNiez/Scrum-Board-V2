7efd26f8e849e86da927fd8f5816758dba9bac3f
Jonathan Niez <116490400+JonathanNiez@users.noreply.github.com>
2023-03-27 08:39:18 +0800

    update3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import Login from "./components/login";
-import Register from "./components/register";
-import { useState } from "react";
-
-export default function Home() {
-  const [page, setPage] = useState("login");
-
-  function changePage(page) {
-    setPage(page);
-  }
-
-  return (
-    <div className="bg-gray-300 w-screen h-screen">
-      <div className="bg-white p-3 drop-shadow-md">
-        <h1 className="text-center text-black text-5xl p-4">
-          Simple Login Form
-        </h1>
-      </div>
-      <div className="flex mt-5 items-center justify-center w-full">
-        <button
-          className="text-black drop-shadow-md w-20 text-center bg-gray-400 rounded hover:bg-gray-500 p-3 m-1"
-          onClick={() => changePage("login")}
-        >
-          Login
-        </button>
-        <button
-          className="text-black drop-shadow-md  w-20 bg-gray-400 rounded hover:bg-gray-500 p-3 m-1"
-          onClick={() => changePage("register")}
-        >
-          Register
-        </button>
-      </div>
-      <div className="flex justify-center space-x-2">
-        {page === "login" && <Login />}
-        {page === "register" && <Register />}
-      </div>
-=======
 import Inputs from "./inputs";
 import Board from "./components/board";
 import React, { useState, useEffect } from "react";
@@ -50,7 +12,6 @@ export default function Home() {
       <div>{<Inputs />}</div>
       <h1 className="text-left text-black text-lg p-4">Scrum Board</h1>
       {<Board />}
->>>>>>> 57d558b (update1)
     </div>
   );
 }

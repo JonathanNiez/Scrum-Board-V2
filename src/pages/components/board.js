2c820d35892f70131dd8c8e00aca869e603a844a
Jonathan Niez <116490400+JonathanNiez@users.noreply.github.com>
2023-03-29 08:25:39 +0800

    test commit

--- src/pages/components/board.js
+++ src/pages/components/board.js
@@ -19,6 +19,11 @@ export default function Board() {
           <p className="font-bold text-lg">Title of Board</p>
           <p>Descrition Here</p>
         </div>
+        <div className="border-gray-300 border-2 rounded-lg p-5">
+          <p className="font-bold text-lg">Test Commit</p>
+          <p>Descrition Here</p>
+        </div>
+
         <Link href="/components/addBoard">
           <div className=" border-gray-300 border-2 rounded-lg p-5">
             <p className="text-3xl text-gray-400 text-center">+</p>{" "}
